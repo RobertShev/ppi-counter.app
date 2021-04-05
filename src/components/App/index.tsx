73a50cb155a1e8226692059db189fa027b4cca72
Robert Sevtsenko <robert.sevstenko@mil.ee>
2021-04-05 17:11:47 +0300

    new clean "quate" template with latest bulma

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,17 @@
-import Navbar from "../Navbar";
 import * as React from "react";
 
 const App: React.FC = () => (
   <>
-    <Navbar />
-    <div className="container">
-      This is a basic app using Typescript, React, Bulma and Font Awesome.
-    </div>
+      <div className="section is-large">
+        <div className="container is-max-desktop">
+            <h1 className="title">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pharetra faucibus nulla, id sodales magna tincidunt et. Integer vel magna."
+            </h1>
+            <h2 className="subtitle">
+                Author
+            </h2>
+        </div>
+      </div>
   </>
 );
 
