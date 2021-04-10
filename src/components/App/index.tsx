8f99f1158032a63d72b39be18b5243c34ac26c48
Robert Sevtsenko <robert.sevstenko@mil.ee>
2021-04-10 11:19:16 +0300

    clean design for quates of day

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,14 +2,23 @@ import * as React from "react";
 
 const App: React.FC = () => (
   <>
-      <div className="section is-large">
+      <div className="section is-large ">
         <div className="container is-max-desktop">
-            <h1 className="title">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pharetra faucibus nulla, id sodales magna tincidunt et. Integer vel magna."
-            </h1>
-            <h2 className="subtitle ">
-                Someone Cool
-            </h2>
+          <div className="columns">
+              <div className="column">1</div>
+              <div className="column">
+                  <h1 className="title">Calculate</h1>
+                  <div className="field">
+                    <input className="input is-rounded" type="text" placeholder="Primary input"/>
+                  </div>
+                  <div className="field">
+                      <input className="input is-rounded" type="text" placeholder="Primary input"/>
+                  </div>
+                  <div className="field">
+                      <input className="input is-rounded" type="text" placeholder="Primary input"/>
+                  </div>
+              </div>
+          </div>
         </div>
       </div>
   </>
